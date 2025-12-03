ab6ffde0ba42c989371904689a185758f03fe502
nguyenphuongthuy58 <nguyenhoangthuy58@gmail.com>
2025-12-03 22:04:42 +0700

    xem ton kho

--- inventory2.py
+++ inventory2.py
@@ -2,8 +2,13 @@
 products = []
 
 def add_product():
-# Nhập tên, giá, số lượng -> append vào products
-print("Đã nhập hàng thành công.")
+ # Nhập tên, giá, số lượng -> append vào products
+ print("Đã nhập hàng thành công.")
+
+def view_inventory():
+ # Duyệt list products và in ra
+ # Ví dụ: Mì tôm - Giá: 5000 - SL: 100
+ pass
 
 def main():
  while True:
